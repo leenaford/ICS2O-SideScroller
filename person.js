80009111b5f4c52785c2eb88dd541a564c7440e9
leenaford <42979902+leenaford@users.noreply.github.com>
2018-11-29 15:25:12 -0500

    Create person.js

---
+++ person.js
@@ -0,0 +1,40 @@
+// Daniel Shiffman
+// https://www.kadenze.com/courses/the-nature-of-code
+// http://natureofcode.com/
+// Session 2: Array of Particles, multiple forces
+
+function Person () {
+  this.pos = createVector(50, height);
+  this.vel = createVector(1, 0);
+  this.acc = createVector(0, 0);
+
+  this.applyForce = function(force) {
+    var f = force.copy();
+    f.div(this.mass);
+    this.acc.add(f);
+  }
+
+  this.update = function() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+  }
+
+  this.display = function() {
+    fill(255);
+    stroke(255);
+  rect(this.pos.x, this.pos.y - 50, 20, 50);
+  }
+
+  this.edges = function() {
+    if (this.pos.y > height) {
+      this.vel.y *= -1;
+      this.pos.y = height;
+    }
+
+    if (this.pos.x > width) {
+      this.vel.x *= -1;
+      this.pos.x = width;
+    }
+  }
+}
