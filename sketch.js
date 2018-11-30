da2bdc089231eb9dffdf5856d6e989fc3bd545b3
leenaford <42979902+leenaford@users.noreply.github.com>
2018-11-30 14:00:13 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,23 +3,33 @@
 // http://natureofcode.com/
 // Session 2: Array of Particles, multiple forces
 
-var particles = [];
-
-var person;
+var man;
 
 function setup() {
-  createCanvas(640, 360);
-	person = new Person();
+  createCanvas(400, 400);
+	man = new Person();
 }
 
 
-
+var x;
+x=-100;
 function draw() {
   background(51);
-	person.update();
-	person.display();
+	
+	if (mouseIsPressed) {
+		var force = createVector(-0.01,0);
+		man.applyForce(force);
+	}
+	
+	translate(-man.pos.x+50, 0);
 	
 	
-  }
-
+													 
+	man.update();
+	man.display();
+	
+	fill(200, 400, 200);
+	rect(250, 350, 100, 100);
+	
+	}
 
